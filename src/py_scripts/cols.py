ba4fc54b34272dc4c3a91ab65c24e47ae9d2a64e
tmasthay <tyler@ices.utexas.edu>
2023-11-08 14:41:10 -0600

    FEATURE: Add columnized output with cols function

---
+++ src/py_scripts/cols.py
@@ -0,0 +1,25 @@
+from tabulate import tabulate as tab
+import sys
+import argparse
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Columnizes a list of words')
+    parser.add_argument('--cols', '-c', type=int, default=10, help='Columns')
+    parser.add_argument('list', nargs='*', help='List of words')
+    args = parser.parse_args()
+
+    full_list = []
+    for e in args.list:
+        if e != '':
+            full_list.extend(e.split())
+
+    final = np.array_split(full_list, len(full_list) // args.cols + 1)
+    final = [list(e) for e in final]
+
+    print(tab(final, tablefmt='plain'))
+
+
+if __name__ == '__main__':
+    main()
