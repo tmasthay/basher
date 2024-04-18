cbe619fd37556c7b74caed9df34a0c23beca811b
tmasthay <tyler@ices.utexas.edu>
2024-04-18 17:47:58 -0500

    REFACTOR: Simplify cla for mrf

--- src/py_scripts/mrf.py
+++ src/py_scripts/mrf.py
@@ -9,32 +9,43 @@ def generate_command():
 
     # Add arguments to the parser
     parser.add_argument(
+        '-p',
         '--path',
         default='$(pwd)',
         help='Starting path for the find command, default to the current working directory.',
     )
     parser.add_argument(
+        '-mid',
         '--mindepth',
         type=int,
         default=1,
         help='Minimum depth for the find command.',
     )
     parser.add_argument(
+        '-mxd',
         '--maxdepth',
         type=int,
         default=5,
         help='Maximum depth for the find command.',
     )
     parser.add_argument(
-        '--count', type=int, default=5, help='Number of files to display.'
+        '-c', '--count', type=int, default=5, help='Number of files to display.'
+    )
+    parser.add_argument(
+        '-n',
+        '--name',
+        type=str,
+        default='',
+        help='Optional regex pattern to match the file names. If supplied, only files with names matching the pattern will be listed.',
     )
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Create the command
+    # Construct the find command string conditionally including name matching
+    name_match_part = f"-name '{args.name}'" if args.name else ''
     command = (
-        f'find "{args.path}" -mindepth {args.mindepth} -maxdepth {args.maxdepth} -type f '
+        f'find "{args.path}" -mindepth {args.mindepth} -maxdepth {args.maxdepth} {name_match_part} -type f '
         f'-exec stat --format=\'%y %n\' {{}} + | sort | tail -n {args.count}'
     )
 
