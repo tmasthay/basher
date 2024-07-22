40ad5d109c53c0a4a5876597cfad6b4dd4138bfd
tmasthay <tyler@ices.utexas.edu>
2024-07-21 19:37:54 -0500

    FEATURE: Make sitecustomize more configurable

--- src/py_scripts/sitecustomize.py
+++ src/py_scripts/sitecustomize.py
@@ -1,48 +1,45 @@
-from rich.console import Console
-from rich.traceback import install
 import os
-import sys
-from shutil import get_terminal_size as gts
-import torch
 
-torch.set_printoptions(profile='short')
+def load_rich():
+    from rich.console import Console
+    from rich.traceback import install
+    import sys
+    from shutil import get_terminal_size as gts
+    from mh.core import set_print_options, torch_stats
 
 
-cols, _ = gts(fallback=(80, 24))
-if cols == 0:
-    cols = os.environ.get('COLUMNS', 80)
+    set_print_options(callback=torch_stats('all'))
 
-filename = os.environ.get('RICH_LOG', '')
-if filename.lower() != 'none':
-    if not filename:
-        file = sys.stdout
-    else:
-        file = open(filename, 'wt')
 
-    console = Console(file=file, force_terminal=True)
-    # install(
-    #     console=console,
-    #     show_locals=True,
-    #     word_wrap=False,
-    #     width=cols,
-    #     locals_max_string=cols,
-    #     locals_max_length=20,
-    # )
+    cols, _ = gts(fallback=(80, 24))
+    if cols == 0:
+        cols = os.environ.get('COLUMNS', 80)
 
-    install(
-        console=console,
-        show_locals=os.environ.get('RICH_LOCALS', '').lower() == 'true',
-        word_wrap=False,
-        width=cols,
-        locals_max_length=10,
-        locals_max_string=cols,
-    )
+    filename = os.environ.get('RICH_LOG', '')
+    if filename.lower() != 'none':
+        if not filename:
+            file = sys.stdout
+        else:
+            file = open(filename, 'wt')
 
-# repo_path = os.environ['REPO']
-# nondefault = ['GitHookEm', 'Experiments']
-# repos = [os.path.join(repo_path, e) for e in os.listdir(repo_path) if not e.startswith('.') and not e in nondefault and os.path.isdir(os.path.join(repo_path, e))]
+        console = Console(file=file, force_terminal=True)
+        # install(
+        #     console=console,
+        #     show_locals=True,
+        #     word_wrap=False,
+        #     width=cols,
+        #     locals_max_string=cols,
+        #     locals_max_length=20,
+        # )
 
-# for repo in repos:
-#     sys.path.append(repo)
+        install(
+            console=console,
+            show_locals=os.environ.get('RICH_LOCALS', '').lower() == 'true',
+            word_wrap=False,
+            width=cols,
+            locals_max_length=10,
+            locals_max_string=cols,
+        )
 
-# The above can simply be done by using "pip install -e ." instead
+if int(os.environ.get('USE_RICH', '0')):
+    load_rich()
